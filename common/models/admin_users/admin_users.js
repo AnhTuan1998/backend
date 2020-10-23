b11e69d7fbf402b02d2f66a578df5b15b680a1e9
tavu <tavu@yfv.finance>
2020-10-23 14:32:10 +0700

    Init foreignKeys, relations

---
+++ common/models/admin_users/admin_users.js
@@ -0,0 +1,17 @@
+/**
+ * @Author tuan.vu.
+ * @Created on 10/23/20.
+ * @Last Modified by: tuan.vu.
+ * @Last Modified time: 10/23/20 11:49.
+ */
+
+'use strict'
+
+const app = require('@server/server')
+let loopbackContext = require('loopback-context')
+const Boom = require('@hapi/boom')
+const speakeasy = require('speakeasy')
+
+module.exports = (AdminUsers) => {
+
+}
